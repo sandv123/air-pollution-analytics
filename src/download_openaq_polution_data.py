be4b840684dcb47cfabe50fe9323b358806ad2ad
Victor Sandomirsky <victor.sandomirsky@gmail.com>
2025-12-29 15:41:43 +0100

    Refactored to simplify data download logic - refactor: moved stuff away to utils.py - refactor: renamed get_measurements() to call_openaq_api() to reflect logic - refactor: updated date-related parameters of call_openaq_api() and it's logic to make it universal - feature: separated main logic into three branches to enable separate processing for locations, historic, and incremental data - feature: finished locations loading logic, paired it with the Databricks jobs and pipelines

--- src/download_openaq_polution_data.py
+++ src/download_openaq_polution_data.py
@@ -9,48 +9,12 @@ import zipfile
 from itertools import product
 from argparse import ArgumentParser
 
-
-class TimeoutErrorExt(openaq.TimeoutError):
-    """
-    Extension to openaq.TimeoutError to push the expected timeout in seconds up the callstack
-    """
-    def __init__(self, e: openaq.TimeoutError, timeout_seconds: float):
-        self.status_code = e.status_code
-        super().__init__(str(e))
-        self.timeout_seconds = timeout_seconds
+from utils import utils
 
 
-class OpenAQConnectionManager:
-    """
-    Wrapper around OpenAQ class to easily restart the connection in case of an endless annoying service timeout loop
-    """
-    client: openaq.OpenAQ | None = None
-
-    def __init__(self, api_key: str):
-        self._api_key = api_key
-
-    
-    def __del(self):
-        if self.client is not None:
-            self.client.close()
 
 
-    def recycle_client(self, timeout: float) -> openaq.OpenAQ:
-        """
-        Get a client object. If exists, cycle it by closing, sleep for {timeout} seconds, and opening anew
-
-        :return: OpenAQ client object
-        :rtype: openaq.OpenAQ
-        """
-        if self.client is not None:
-            self.client.close()
-            time.sleep(timeout)
-        
-        client = openaq.OpenAQ(self._api_key)
-        return client
-
-
-def get_measurements(client: openaq.OpenAQ, sensor_id: int, year: int, page_num: int) -> dict[str, Any]:
+def call_openaq_api(client: openaq.OpenAQ, sensor_id: int, date_from: str, date_to: str, page_num: int) -> dict[str, Any]:
     """
     Run a single call to the OpenAQ API to get measurements for a particular parameter for the whole year.
     The service uses paging API so the method retrieves a single page of the results.
@@ -60,8 +24,10 @@ def get_measurements(client: openaq.OpenAQ, sensor_id: int, year: int, page_num:
     :type client: openaq.OpenAQ
     :param sensor_id: Id of the sensor to query
     :type sensor_id: int
-    :param year: The year to retrieve measurements for
-    :type year: int
+    :param date_from: Start of the retrieval period
+    :type year: str
+    :param date_to: End of the retrieval period
+    :type year: str
     :param page_num: Result page to retrieve
     :type page_num: int
     :return: Parsed JSON response
@@ -79,10 +45,12 @@ def get_measurements(client: openaq.OpenAQ, sensor_id: int, year: int, page_num:
     try:
         # Call the OpenAQ API
         measurements = client.measurements.list(
-            sensors_id=sensor_id,
-            datetime_from=f'{year}-01-01',
-            datetime_to=f'{year}-12-31',
-            limit=limit, page=page_num)
+            sensors_id=int(sensor_id), 
+            datetime_from=date_from, 
+            datetime_to=date_to, 
+            limit=limit, 
+            page=page_num
+        )
         # print(f'  page={page_num} len={len(measurements.results)}', measurements.headers)
         
         # Pages are retrieved until current page returns zero results
@@ -109,28 +77,6 @@ def get_measurements(client: openaq.OpenAQ, sensor_id: int, year: int, page_num:
     return result
 
 
-def is_running_in_databricks() -> bool:
-    return "DATABRICKS_RUNTIME_VERSION" in os.environ
-
-
-def setup_environment() -> tuple[str, str]:
-    # OpenAQ API key
-    if is_running_in_databricks():
-        print("Running inside Databricks")
-        argparse = ArgumentParser()
-        argparse.add_argument("--datastore_path", type=str)
-        args = argparse.parse_args()
-
-        api_key = dbutils.secrets.get(scope = "air-polution-analytics APIs keys", key = "OPENAQ_API_KEY") # type: ignore
-        datastore = args.datastore_path
-    else:
-        print("Running inside IDE")
-        api_key = os.environ['OPENAQ_API_KEY']
-        datastore = os.environ['DATASTORE_PATH']
-
-    return (api_key, datastore)
-
-
 def mark_processed_chunk(datastore: str, name: str, tag: str) -> None:
     with open(datastore + f'{name}.{tag}', 'w') as f:
         f.write("done")
@@ -141,111 +87,149 @@ def check_processed_chunk(datastore: str, name: str, tag: str) -> bool:
     return os.path.exists(datastore + f'{name}.{tag}')
 
 
-def main():
-    api_key, datastore = setup_environment()
+def download_chunk(config, connection_mgr, locationid, sensorid, date_from, date_to):
+    client = connection_mgr.get_client()
 
-    connection_mgr = OpenAQConnectionManager(api_key)
-    client = connection_mgr.recycle_client(0)
+    chunk_name = f'{locationid}_{sensorid}_{date_from}_{date_to}'
+    print(f'  Downloading chunk {chunk_name}')
+    if check_processed_chunk(config["datastore"], chunk_name, 'finished'):
+        print(f'  Chunk {chunk_name} already downloaded, skipping')
+        return
+    
+    timed_out = 0
+    page_num = 0
+    while True:
+        page_num += 1
+        filename = f'{chunk_name}_page{page_num}.json'
 
-    # Geo coords for Belgrade, RS
-    belgrade_coordinates = (44.8125, 20.4612)
+        # If a page has already been downloaded, skip it
+        if os.path.exists(config["datastore"] + filename + '.zip'):
+            print(f'    Page {filename}.zip exists, skipping')
+            continue
 
+        try:
+            page = call_openaq_api(client, sensorid, date_from, date_to, page_num)
+            timed_out = 0
+            if not page:
+                break
+
+            # Store the data in zipped format. This shrinks the files about 50 times
+            utils.store_zipped(config["datastore"], filename, json.dumps(page, indent=4))
+        except utils.TimeoutErrorExt as e:
+            if timed_out == 3:
+                print(f'  Timed out (API) 3 times, skipping page {filename}')
+                mark_processed_chunk(config["datastore"], filename, 'skipped_page')
+                client = connection_mgr.recycle_client(0)
+                timed_out += 1
+                continue
+            if timed_out == 5:
+                print(f'  Timed out (API) 5 times, skipping chunk {chunk_name}')
+                mark_processed_chunk(config["datastore"], filename, 'skipped_chunk')
+                client = connection_mgr.recycle_client(0)
+                timed_out += 1
+                break
+            if timed_out == 7:    
+                # If the API timed out 7 times in a row, cancel trying and bail the whole thing
+                print('  Timed out (API) 7 times, canceling')
+                raise e
+            else:
+                # If the API timed out, try resetting the connection and wait for the API suggested amount of seconds
+                time_to_sleep = e.timeout_seconds + 60 * timed_out
+                print(f'TIMED OUT OPENAQ, sleeping for {time_to_sleep} seconds, {e}')
+                timed_out += 1
+                client = connection_mgr.recycle_client(time_to_sleep)
+                page_num -= 1
+        except httpcore.ReadTimeout as e:
+            print(f'TIMED OUT HTTP READ, sleeping for 60 seconds, {e}')
+            client = connection_mgr.recycle_client(60)
+            page_num -= 1
+        except TimeoutError as e:
+            print(f'TIMED OUT NETWORK/IO, sleeping for 60 seconds, {e}')
+            client = connection_mgr.recycle_client(60)
+            page_num -= 1
+    mark_processed_chunk(config["datastore"], chunk_name, 'finished')
+
+
+def retrieve_historic_data(config: dict[str, Any], connection_mgr: utils.OpenAQConnectionManager):
+    coordinates: dict[str, tuple[float, float]] = config["coordinates"]
+
+    city = config['city']
     # Retrieve data for these years to analyze
-    years = [2021, 2022, 2023, 2024, 2025]
+    years = config["backfill_years"]
 
     # Retrieve measurements for these parameters of the sensors
-    parameters = ['pm1', 'pm10', 'pm25', 'temperature']
-
-    # Find all available locations in the radius of 8 km around Belgrade center
-    locations = client.locations.list(coordinates=belgrade_coordinates, radius=8000, limit=1000)
-
-    # locations = client.locations.get(2812630) # This is for debugging purposes
-
-    # Store the locations for processing
-    store_zipped(datastore, 'Belgrade-locations.json', json.dumps(json.loads(locations.json()), indent=4))
-    print(f'Got {len(locations.results)} locations')
+    parameters = config["parameters"]
 
     for l in locations.results:
-        name = l.name.replace(' ', '-')
+        location_name = l.name.replace(' ', '-')
 
         # For current location find the ids of the sensors that provide measurements for parameters we are interested in
         # If no sensor measures interesting parameter, skip the parameter
         sensors_parameters = {s.parameter.name: s.id  for s in l.sensors}
         sensor_ids = filter(lambda f: f != 0, map(lambda x: sensors_parameters.get(x, 0), parameters))
-        print(f'Downloading measurements for station {name}')
-
-        # years = [2025] # This is for debugging purposes
+        print(f'Downloading measurements for location {location_name}')
         
-        timed_out = 0
         for year_and_sensor in product(years, sensor_ids):
-            year_and_sensor_name = f'{l.id}_{year_and_sensor[1]}_{l.country.code}_{name}_{year_and_sensor[0]}'
-            print(f'  Downloading chunk {year_and_sensor_name}')
-            if check_processed_chunk(datastore, year_and_sensor_name, 'finished'):
-                print(f'  Chunk {year_and_sensor_name} already downloaded, skipping')
-                continue
-
-            page_num = 0
-            while True:
-                page_num += 1
-                filename = f'{year_and_sensor_name}_page{page_num}.json'
-
-                # If a page has already been downloaded, skip it
-                if os.path.exists(datastore + filename + '.zip'):
-                    print(f'    Page {filename}.zip exists, skipping')
-                    continue
-
-                try:
-                    page = get_measurements(client, year_and_sensor[1], year_and_sensor[0], page_num)
-                    timed_out = 0
-                    if not page:
-                        break
-
-                    # Store the data in zipped format. This shrinks the files about 50 times
-                    store_zipped(datastore, filename, json.dumps(page, indent=4))
-                except TimeoutErrorExt as e:
-                    if timed_out == 3:
-                        print(f'  Timed out (API) 3 times, skipping page {filename}')
-                        mark_processed_chunk(datastore, filename, 'skipped_page')
-                        client = connection_mgr.recycle_client(0)
-                        timed_out += 1
-                        continue
-                    if timed_out == 5:
-                        print(f'  Timed out (API) 5 times, skipping chunk {name}')
-                        mark_processed_chunk(datastore, filename, 'skipped_chunk')
-                        client = connection_mgr.recycle_client(0)
-                        timed_out += 1
-                        break
-                    if timed_out == 7:    
-                        # If the API timed out 7 times in a row, cancel trying and bail the whole thing
-                        print('  Timed out (API) 7 times, canceling')
-                        raise e
-                    else:
-                        # If the API timed out, try resetting the connection and wait for the API suggested amount of seconds
-                        time_to_sleep = e.timeout_seconds + 60 * timed_out
-                        print(f'TIMED OUT OPENAQ, sleeping for {time_to_sleep} seconds, {e}')
-                        timed_out += 1
-                        client = connection_mgr.recycle_client(time_to_sleep)
-                        page_num -= 1
-                except httpcore.ReadTimeout as e:
-                    print(f'TIMED OUT HTTP READ, sleeping for 60 seconds, {e}')
-                    client = connection_mgr.recycle_client(60)
-                    page_num -= 1
-                except TimeoutError as e:
-                    print(f'TIMED OUT NETWORK/IO, sleeping for 60 seconds, {e}')
-                    client = connection_mgr.recycle_client(60)
-                    page_num -= 1
-            mark_processed_chunk(datastore, year_and_sensor_name, 'finished')
+            download_chunk(config,
+                           connection_mgr,
+                           l.id,
+                           year_and_sensor[1],
+                           f"{year_and_sensor[0]}-01-01",
+                           f"{year_and_sensor[0]}-12-31")
 
 
-def store_zipped(datastore:str, filename: str, data: str):
+def retrieve_recent_data(config: dict[str,Any], connection_mgr: utils.OpenAQConnectionManager):
+    coordinates: tuple[float, float] = (config["latitude"], config["longitude"])
 
-    zipped_content = io.BytesIO()
-    with zipfile.ZipFile(zipped_content, 'w', zipfile.ZIP_DEFLATED, compresslevel=9) as file:
-        file.writestr(filename, data)
+    date_from = config["date_from"]
+    date_to = config["date_to"]
 
-    with open(datastore + filename + ".zip", "wb") as f:
-        f.write(zipped_content.getvalue())
+    # Retrieve measurements for these parameters of the sensors
+    parameters = config["parameters"]
+
+    for s in config["locations_sensors"]:
+        locationid, sensorid = s.split("_")
+        print(f"Downloading data for location {locationid} sensor {sensorid}, from {date_from} to {date_to}")
+        
+        download_chunk(config,
+                       connection_mgr,
+                       locationid,
+                       sensorid,
+                       date_from,
+                       date_to)
+
+
+def retrieve_locations(config: dict[str,Any], connection_mgr: utils.OpenAQConnectionManager):
+    coordinates: tuple[float, float] = (config["latitude"], config["longitude"])
+    city: str = config["city"]
+
+    # Find all available locations in the radius of 8 km around Belgrade center
+    locations = connection_mgr.get_client().locations.list(coordinates=coordinates, radius=8000, limit=1000)
+
+    # Store the locations for processing
+    utils.store_file(
+        config["datastore_path"],
+        f'{city}-locations.json',
+        json.dumps(json.loads(locations.json()), indent=4),
+        compression="gzip")
+    print(f'Got {len(locations.results)} locations')
 
 
 if __name__ == "__main__":
-    main()
+    if not utils.is_running_in_databricks():
+        dbutils = None
+    config = utils.setup_environment(dbutils) # type: ignore
+
+    print(config)
+
+    connection_mgr = utils.OpenAQConnectionManager(config["api_key"])
+    if config["mode"] =="backfill":
+        if not config.get("city") or not config.get("latitude") or not config.get("longitude") or not config.get("datastore_path"):
+            raise ValueError("Parameters are required: city, latitude, longitude, datastore_path")
+        retrieve_historic_data(config, connection_mgr)
+    elif config["mode"] == "locations":
+        if not config.get("city") or not config.get("latitude") or not config.get("longitude") or not config.get("datastore_path"):
+            raise ValueError("Parameters are required: city, latitude, longitude, datastore_path")
+        retrieve_locations(config, connection_mgr)
+    else:
+        retrieve_recent_data(config, connection_mgr)
