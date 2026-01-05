33739bd105b9826dfe60776e6da759d9379d7a87
Victor Sandomirsky <victor.sandomirsky@gmail.com>
2026-01-05 15:03:32 +0100

    Added deduplication for OpenAQ measurements - feature: added script to dedup measurements - feature: updated ingestion job to run dedup after ingesting the data

---
+++ src/dedup_openaq_measurements.py
@@ -0,0 +1,43 @@
+from pyspark.sql.functions import col, current_timestamp, concat, sha2, split # type: ignore
+from delta.tables import * # type: ignore
+
+catalog = "air_polution_analytics_dev"
+landing_schema = "00_landing"
+silver_schema = "02_silver"
+source_table = "openaq_measurements"
+target_table = "openaq_measurements_deduped"
+
+base_path = f"/Volumes/{catalog}/{silver_schema}/metadata"
+openaq_path = f"{base_path}/openaq"
+
+create_table = f"create table if not exists {target_table} (id STRING, datetime_from TIMESTAMP, value FLOAT, location_id INT, sensor_id INT);"
+create_volume = f"create volume if not exists {catalog}.{silver_schema}.metadata;"
+dbutils.fs.mkdirs(openaq_path)
+
+spark.sql(create_table)
+spark.sql(create_volume)
+
+def upsertToDelta(microBatchOutputDF, batchId):
+    tableDeduped = DeltaTable.forName(spark, f"{catalog}.{silver_schema}.{target_table}")
+    (tableDeduped.alias("t").merge(
+        microBatchOutputDF.alias("s"),
+        "s.id = t.id")
+    .whenNotMatchedInsertAll()
+    .execute()
+    )
+
+    
+split_col = split(col("source_file_name"), "_")
+    
+df = (spark.readStream
+    .table(f"{catalog}.{silver_schema}.{source_table}")
+    .withColumn("id", sha2(concat(col("sensor_id"), col("location_id"), col("datetime_from")), 256))
+)
+
+(df.writeStream
+  .foreachBatch(upsertToDelta)
+  .outputMode("update")
+  .trigger(availableNow=True)
+  .option("checkpointLocation", f"{openaq_path}/_schema")
+  .start()
+)
