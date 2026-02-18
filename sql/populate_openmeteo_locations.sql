f59563cba3f4bec067c236cb992b2d42edfca152
Victor Sandomirsky <victor.sandomirsky@gmail.com>
2026-02-18 12:11:53 +0100

    Updated weather locations to star schema - refactor: renamed temperature_locations to dim_weather_locations - fix: deduplicated weather locations

--- sql/populate_openmeteo_locations.sql
+++ sql/populate_openmeteo_locations.sql
@@ -1,25 +1,29 @@
 USE CATALOG IDENTIFIER(:catalog);
 USE IDENTIFIER(:schema);
 
-create table if not exists temperature_locations (
-  location_id string,
-  latitude float,
-  longitude float,
-  city string,
+create table if not exists dim_weather_locations (
+  location_id string PRIMARY KEY,
+  latitude float NOT NULL,
+  longitude float NOT NULL,
+  city string NOT NULL,
   timezone string,
   elevation float
 );
 
-merge into temperature_locations as target
+merge into dim_weather_locations as target
 using (
-  select
-    distinct sha2(concat(latitude, "|", longitude), 256) as location_id,
-    latitude,
-    longitude,
-    split(source_file_name, "_")[1] as city,
-    timezone,
-    elevation
-  from air_polution_analytics_dev.`01_bronze`.temperature_measurements_raw
+  select *
+  from (
+    select
+      sha2(concat(latitude, "|", longitude), 256) as location_id,
+      latitude,
+      longitude,
+      split(source_file_name, "_")[1] as city,
+      timezone,
+      elevation,
+      row_number() over(partition by sha2(concat(latitude, "|", longitude), 256) order by bronze_load_ts desc) as row_num
+    from air_polution_analytics_dev.`01_bronze`.temperature_measurements_raw
+  ) where row_num = 1
 ) as source
 on target.location_id = source.location_id
 when not matched then insert *
